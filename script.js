fc0b1c745c4953f8c991fa04500ac66da633844c
Alan Timbó <alancorreiatimbo@gmail.com>
2022-09-12 12:34:16 -0300

    Todas as funções lógicas do site estão operantes, adicionadas: atualização da página e inclusão do último pedido no rodapé, últimos 10 pedidos a mostra e clicáveis, envio de novo pedido após clicar em um já existente

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ if (nome.length === 0) {
     }
 }
 
+// let nome = "abc";
+
 let modelo;
 let gola;
 let tecido;
 let link = "";
 
-// função concluída
+
+// Seleciona o tipo de modelo
 function selecionaOpcaoDeModelo(click) {
     const opcaoSelecionada = document.querySelector(".modelos .selecionado");
 
@@ -27,7 +30,7 @@ function selecionaOpcaoDeModelo(click) {
     habilitaBotao();
 }
 
-// função concluída
+// Seleciona o tipo de gola
 function selecionaOpcaoDeGola(click) {
     const opcaoSelecionada = document.querySelector(".golas .selecionado");
 
@@ -41,7 +44,7 @@ function selecionaOpcaoDeGola(click) {
     habilitaBotao();
 }
 
-// função concluída
+// Seleciona o tipo de tecido
 function selecionaOpcaoDeTecido(click) {
     const opcaoSelecionada = document.querySelector(".tecidos .selecionado");
 
@@ -55,7 +58,7 @@ function selecionaOpcaoDeTecido(click) {
     habilitaBotao();
 }
 
-// função concluída
+// confere e armazena o link
 function recebeLink(valor) {
     link = valor;
     if (link[0] === "h" && link[1] === "t" && link[2] === "t" && link[3] === "p" && link[4] === "s" && link[5] === ":" && link[6] === "/" && link[7] === "/" && link.length > 15) {
@@ -67,7 +70,7 @@ function recebeLink(valor) {
     }
 }
 
-// função concluída
+// habilita o botão após confirmação das etapas anteriores
 function habilitaBotao() {
     const botaoConfirmacao = document.querySelector(".botao");
 
@@ -92,10 +95,9 @@ function habilitaBotao() {
                         };
 
                         console.log(objeto);
-
+                        // Dados preenchidos enviados para o axios.post
                         if (botaoConfirmacao.classList.contains("botao-clicado")) {
                             const button = document.querySelector("button");
-
                             button.onclick = function () { enviaRequisicao(objeto) };
                         }
                     }
@@ -105,7 +107,7 @@ function habilitaBotao() {
     }
 }
 
-// função concluída
+// Envia a requisição após clicar no botão
 function enviaRequisicao(objeto) {
     console.log(objeto);
 
@@ -117,15 +119,19 @@ function enviaRequisicao(objeto) {
 // função concluída
 function encomendaConfirmada(dadosEnviados) {
     console.log(dadosEnviados.data);
+    // Confirmação do novo pedido criado
     let pedido = dadosEnviados.data;
     alert(`Parabéns, ${nome}, sua encomenda foi confirmada!\nVocê escolheu uma blusa do modelo ${pedido.model}, de tecido ${pedido.material} e gola ${pedido.neck}.`);
+    document.location.reload(true);
 }
 
+// Mensagem em caso de erro no processamento da encomenda + retorna para o início
 function erroNaEncomenda(dadosEnviados) {
     alert("Ops, não conseguimos processar sua encomenda");
-    // recarregar página!!
+    document.location.reload(true);
 }
 
+// Busca as encomendas do servidor ao inicializar o servidor
 fazRequisicao();
 function fazRequisicao() {
     const promessa = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts");
@@ -133,7 +139,53 @@ function fazRequisicao() {
     promessa.catch(erroNaEncomenda);
 }
 
+// Recolhe os dados ao iniciar/atualizar a página para exibição no rodapé
 function mostraRequisicao(dadosRecebidos) {
-    console.log(dadosRecebidos);
-    // mostrar na tela
+    let recebidos = dadosRecebidos.data;
+    console.log(recebidos);
+
+    const ultimosPedidos = document.querySelector(".ultimos-pedidos");
+    // expõe no rodapé da página os últimos 10 pedidos finalizados
+    let camisaRecente = "";
+    for (let i = 0; i < 10; i++) {
+        console.log(recebidos[i]);
+        camisaRecente += `<div class="camisa-div" id="${recebidos[i].id},${recebidos[i].model},${recebidos[i].neck},${recebidos[i].material},${recebidos[i].owner},${recebidos[i].image}" onclick="novaRequisicao(this)">
+    <img src=${recebidos[i].image} alt="">
+    <h3><b>Criador:</b> ${recebidos[i].owner} </h3>
+</div>`;
+    }
+    ultimosPedidos.innerHTML += camisaRecente;
+}
+
+// Seleciona os dados do pedido clicado e os reencaminha para o servidor, sob novo author
+function novaRequisicao(data) {
+    console.log(data);
+    let idClicado = data.id;
+    console.log(idClicado);
+
+    array = idClicado.split(",");
+    console.log(array);
+
+    let objetoPedido = {
+        "model": `${array[1]}`,
+        "neck": `${array[2]}`,
+        "material": `${array[3]}`,
+        "image": `${array[5]}`,
+        "owner": `${array[4]}`,
+        "author": `${nome}`
+    };
+
+    console.log(objetoPedido);
+
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", objetoPedido);
+    promessa.then(novaEncomenda);
+    promessa.catch(erroNaEncomenda);
+}
+
+// Confirma o pedido realizado após clicar em algum já existente
+function novaEncomenda(dadosReenviados) {
+    console.log(dadosReenviados.data);
+    let pedido = dadosReenviados.data;
+    confirm(`Parabéns, ${nome}, sua encomenda foi confirmada!\nVocê escolheu uma blusa do modelo ${pedido.model}, de tecido ${pedido.material} e gola ${pedido.neck}.`);
+    document.location.reload(true);
 }
