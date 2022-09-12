bd3946ab5fdc729d4bc2e44670906e5f53c1da49
Alan Timbó <alancorreiatimbo@gmail.com>
2022-09-12 14:40:00 -0300

    projeto funcional, layout e parte lógica terminados

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let nome = prompt("Digite aqui seu nome: ");
 
-if (nome.length === 0) {
-
-    for (let i = 1; nome.length < i; i += 0) {
-        alert("Por favor, insira um nome válido.");
-        nome = prompt("Digite aqui seu nome: ");
-    }
+while (nome === null || nome.length === 0) {
+    alert("Por favor, insira um nome válido.");
+    nome = prompt("Digite aqui seu nome: ");
 }
-
-// let nome = "abc";
 
 let modelo;
 let gola;
@@ -26,7 +21,6 @@ function selecionaOpcaoDeModelo(click) {
 
     click.classList.add("selecionado");
     modelo = click.id;
-    console.log(modelo);
     habilitaBotao();
 }
 
@@ -40,7 +34,6 @@ function selecionaOpcaoDeGola(click) {
 
     click.classList.add("selecionado");
     gola = click.id;
-    console.log(gola);
     habilitaBotao();
 }
 
@@ -54,7 +47,6 @@ function selecionaOpcaoDeTecido(click) {
 
     click.classList.add("selecionado");
     tecido = click.id;
-    console.log(tecido);
     habilitaBotao();
 }
 
@@ -62,7 +54,6 @@ function selecionaOpcaoDeTecido(click) {
 function recebeLink(valor) {
     link = valor;
     if (link[0] === "h" && link[1] === "t" && link[2] === "t" && link[3] === "p" && link[4] === "s" && link[5] === ":" && link[6] === "/" && link[7] === "/" && link.length > 15) {
-        console.log(link);
         habilitaBotao();
     }
     else {
@@ -79,9 +70,6 @@ function habilitaBotao() {
             if (gola !== undefined) {
                 if (tecido !== undefined) {
                     if (link !== "") {
-
-                        console.log(link, modelo, gola, tecido, nome);
-
                         botaoConfirmacao.classList.remove("botao-desativado");
                         botaoConfirmacao.classList.add("botao-clicado");
 
@@ -94,7 +82,6 @@ function habilitaBotao() {
                             "author": `${nome}`
                         };
 
-                        console.log(objeto);
                         // Dados preenchidos enviados para o axios.post
                         if (botaoConfirmacao.classList.contains("botao-clicado")) {
                             const button = document.querySelector("button");
@@ -109,20 +96,18 @@ function habilitaBotao() {
 
 // Envia a requisição após clicar no botão
 function enviaRequisicao(objeto) {
-    console.log(objeto);
-
     const promessa = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", objeto);
     promessa.then(encomendaConfirmada);
     promessa.catch(erroNaEncomenda);
 }
 
-// função concluída
+// Encaminha dados para o servidor
 function encomendaConfirmada(dadosEnviados) {
-    console.log(dadosEnviados.data);
     // Confirmação do novo pedido criado
     let pedido = dadosEnviados.data;
     alert(`Parabéns, ${nome}, sua encomenda foi confirmada!\nVocê escolheu uma blusa do modelo ${pedido.model}, de tecido ${pedido.material} e gola ${pedido.neck}.`);
     document.location.reload(true);
+    // atualizar rodapé
 }
 
 // Mensagem em caso de erro no processamento da encomenda + retorna para o início
@@ -142,14 +127,12 @@ function fazRequisicao() {
 // Recolhe os dados ao iniciar/atualizar a página para exibição no rodapé
 function mostraRequisicao(dadosRecebidos) {
     let recebidos = dadosRecebidos.data;
-    console.log(recebidos);
 
     const ultimosPedidos = document.querySelector(".ultimos-pedidos");
     // expõe no rodapé da página os últimos 10 pedidos finalizados
     let camisaRecente = "";
     for (let i = 0; i < 10; i++) {
-        console.log(recebidos[i]);
-        camisaRecente += `<div class="camisa-div" id="${recebidos[i].id},${recebidos[i].model},${recebidos[i].neck},${recebidos[i].material},${recebidos[i].owner},${recebidos[i].image}" onclick="novaRequisicao(this)">
+        camisaRecente += `<div class="camisa-div" data-dados="${recebidos[i].id},${recebidos[i].model},${recebidos[i].neck},${recebidos[i].material},${recebidos[i].owner},${recebidos[i].image}" onclick="novaRequisicao(this)">
     <img src=${recebidos[i].image} alt="">
     <h3><b>Criador:</b> ${recebidos[i].owner} </h3>
 </div>`;
@@ -159,23 +142,17 @@ function mostraRequisicao(dadosRecebidos) {
 
 // Seleciona os dados do pedido clicado e os reencaminha para o servidor, sob novo author
 function novaRequisicao(data) {
-    console.log(data);
-    let idClicado = data.id;
-    console.log(idClicado);
-
+    let idClicado = data.getAttribute("data-dados");
     array = idClicado.split(",");
-    console.log(array);
 
     let objetoPedido = {
         "model": `${array[1]}`,
         "neck": `${array[2]}`,
         "material": `${array[3]}`,
         "image": `${array[5]}`,
-        "owner": `${array[4]}`,
-        "author": `${nome}`
+        "owner": `${nome}`,
+        "author": `${array[4]}`
     };
-
-    console.log(objetoPedido);
 
     const promessa = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", objetoPedido);
     promessa.then(novaEncomenda);
@@ -184,8 +161,8 @@ function novaRequisicao(data) {
 
 // Confirma o pedido realizado após clicar em algum já existente
 function novaEncomenda(dadosReenviados) {
-    console.log(dadosReenviados.data);
     let pedido = dadosReenviados.data;
     confirm(`Parabéns, ${nome}, sua encomenda foi confirmada!\nVocê escolheu uma blusa do modelo ${pedido.model}, de tecido ${pedido.material} e gola ${pedido.neck}.`);
     document.location.reload(true);
+    // atualizar rodapé
 }
